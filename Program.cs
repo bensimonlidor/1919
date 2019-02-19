025e73c4669807ebfe540782f4ea7839d10bf6c6
bensimonlidor <47791397+bensimonlidor@users.noreply.github.com>
2019-02-19 19:57:57 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,6 +10,18 @@ namespace lid1902
 	{
 		static void Main(string[] args)
 		{
+			int a, b;
+			Console.WriteLine("Please enter 2 numbers:");
+			a = Convert.ToInt32(Console.ReadLine());
+			b = Convert.ToInt32(Console.ReadLine());
+			if (a > b)
+			{
+				Console.WriteLine(a);
+			}
+			else
+			{
+				Console.WriteLine(b);
+			}
 		}
 	}
 }
